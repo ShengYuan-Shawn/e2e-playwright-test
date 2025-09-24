25dd6809d2d7ac8a4fd34e2734b09790918a1bc9
ShengYuan-Shawn <shengyuan1103@gmail.com>
2025-09-24 22:04:26 +0800

    login test

--- tests/login-test.spec.ts
+++ tests/login-test.spec.ts
@@ -5,26 +5,61 @@ import { LoginPage } from "../pages/LoginPage";
 test.describe("Login Functionality", () => {
   const baseURL = process.env.BASE_URL || "https://www.saucedemo.com/";
 
+  let commonUtils: CommonUtils;
+  let loginPage: LoginPage;
+
+  // Setup that runs before each test
+  test.beforeEach(async ({ page }) => {
+    commonUtils = new CommonUtils(page);
+    loginPage = new LoginPage(page);
+    await commonUtils.goTo(baseURL);
+  });
+
   test("Verify Login Page UI", async ({ page }) => {
-    const commonUtils = new CommonUtils(page);
     await commonUtils.goTo(baseURL);
-
-    const loginPage = new LoginPage(page);
-    await loginPage.verifyLoginPageUI();
+    await loginPage.verifyLoginPage();
   });
 
-  test("Verify Login With Invalid User", async ({ page }) => {
-    const commonUtils = new CommonUtils(page);
+  test("Verify Login With Invalid Username & Password", async ({ page }) => {
     await commonUtils.goTo(baseURL);
 
-    const loginPage = new LoginPage(page);
-    await loginPage.loginInvalidUser();
+    // Validate Login With Name Only
+    await loginPage.clearUsernameInput();
+    await loginPage.inputRandomUsername();
+    await loginPage.loginUser();
+    await loginPage.errorMessageValidation("password");
+
+    await loginPage.clearUsernameInput();
+
+    // Validate Login With Password Only
+    await loginPage.clearPasswordInput();
+    await loginPage.inputRandomPassword();
+    await loginPage.loginUser();
+    await loginPage.errorMessageValidation("username");
+
+    await loginPage.clearPasswordInput();
+
+    // Validate Login With Invalid Username & Password
+    await loginPage.inputRandomUsername();
+    await loginPage.inputRandomPassword();
+    await loginPage.loginUser();
+    await loginPage.errorMessageValidation();
   });
 
-  // test("Verify Login With Valid User", async ({ page }) => {
-  //   const commonUtils = new CommonUtils(page);
-  //   await commonUtils.goTo(baseURL);
+  test("Verify Login With Locked Username & Password", async ({ page }) => {
+    await commonUtils.goTo(baseURL);
 
-  //   const loginPage = new LoginPage(page);
-  // });
+    await loginPage.inputValidUsername(process.env.LOCKED_USER);
+    await loginPage.inputValidPassword(process.env.VALID_PASSWORD);
+    await loginPage.loginUser();
+    await loginPage.errorMessageValidation("locked");
+  });
+
+  test("Verify Login With Valid Username & Password", async ({ page }) => {
+    await commonUtils.goTo(baseURL);
+
+    await loginPage.inputValidUsername(process.env.VALID_USER);
+    await loginPage.inputValidPassword(process.env.VALID_PASSWORD);
+    await loginPage.loginUser();
+  });
 });
