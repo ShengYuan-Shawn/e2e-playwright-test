131a00562ae3d4439fe4f554565e4174e02731ae
ShengYuan-Shawn <shengyuan1103@gmail.com>
2026-02-28 00:47:29 +0800

    update keys

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   reporter: [
     ['html'], 
     ['allure-playwright'],
+    ['@ctrf/playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
