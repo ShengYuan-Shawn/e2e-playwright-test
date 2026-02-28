6c94373343703db93796e3cabec99d6e9b2f9ac3
ShengYuan-Shawn <shengyuan1103@gmail.com>
2026-02-28 14:36:46 +0800

    update report

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   reporter: [
     ['html'], 
     ['allure-playwright'],
+    ['json', { outputFile: 'test-results/results.json' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
